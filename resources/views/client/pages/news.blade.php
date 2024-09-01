654b91f0ee2aa515732c9d644f9920dda92436ce
zaki <zaki@pratama.tech>
2024-09-01 09:35:55 +0700

    ;;

--- resources/views/client/pages/news.blade.php
+++ resources/views/client/pages/news.blade.php
@@ -25,30 +25,31 @@
                     <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12">
                         <div class="crs_grid">
                             <div class="crs_grid_thumb">
-                                <a href="{{ url('news/' . $item->slug) }}" class="crs_detail_link">
-                                    <img src="{{ asset('storage/assets/attach/' . $item->image) }}"
-                                        class="img-cover rounded" />
+                                <a href="https://www.youtube.com/watch?v=69BninGgjaA&ab_channel=PurwakartaGaluhpakuanTV"
+                                    target="_blank" class="crs_detail_link">
+                                    <img src="https://img.youtube.com/vi/69BninGgjaA/hqdefault.jpg"
+                                        class="img-fluid rounded image-full"
+                                        alt="PERTAHANKAN ANGKA PERTUMBUHAN PENDUDUK, KB UNTUK PRIA DIGALAKAN">
                                 </a>
                             </div>
                             <div class="crs_grid_caption">
                                 <div class="crs_flex">
                                     <div class="crs_fl_first">
-                                        <div class="crs_cates cl_8"><span>Berita</span></div>
+                                        <div class="crs_cates cl_8"><span>Video</span></div>
                                     </div>
                                     <div class="crs_fl_last">
-                                        <div class="crs_inrolled"><strong>{{ $item->comments->count() }}</strong>Komentar
-                                        </div>
+                                        <div class="crs_inrolled"><strong>0</strong>Melihat</div>
                                     </div>
                                 </div>
                                 <div class="crs_title">
-                                    <h4><a href="{{ url('news/' . $item->slug) }}"
-                                            class="crs_title_link overflow">{{ $item->name }}</a></h4>
+                                    <h4><a title="PERTAHANKAN ANGKA PERTUMBUHAN PENDUDUK, KB UNTUK PRIA DIGALAKAN"
+                                            href="https://www.youtube.com/watch?v=69BninGgjaA&ab_channel=PurwakartaGaluhpakuanTV"
+                                            target="_blank" class="crs_title_link overflow">PERTAHANKAN ANGKA PERTUMBUHAN
+                                            PENDUDUK, KB UNTUK PRIA DIGALAKAN</a></h4>
                                 </div>
                                 <div class="crs_info_detail">
                                     <ul>
-                                        <li><i
-                                                class="fa fa-clock text-danger"></i><span>{{ timesInd($item->created_at) }}</span>
-                                        </li>
+                                        <li><i class="fa fa-clock text-danger"></i><span>15 Maret 2021 09:04</span></li>
                                     </ul>
                                 </div>
                             </div>
@@ -56,7 +57,7 @@
                                 <div class="crs_flex">
                                     <div class="crs_fl_first">
                                         <div class="crs_tutor">
-                                            <div class="crs_tutor_name">{{ $item->user->name }}</div>
+                                            <div class="crs_tutor_name">Administrator</div>
                                         </div>
                                     </div>
                                 </div>
